0ea4fadd4ab856d61fe58e3e9b5aea2c6af5538c
Antoine Barrault <barrault01@gmail.com>
2022-12-06 07:12:03 +0100

    Create project

--- DetectOrientationOnLaunch/ViewController.swift
+++ DetectOrientationOnLaunch/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var orientationLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        if traitCollection.verticalSizeClass == UIUserInterfaceSizeClass.compact {
+            orientationLabel.text = "landscape"
+        }
+        
+        if traitCollection.verticalSizeClass == .regular {
+            orientationLabel.text = "portrait"
+        }
         // Do any additional setup after loading the view.
     }
 
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if traitCollection.verticalSizeClass == UIUserInterfaceSizeClass.compact {
+            orientationLabel.text = "landscape"
+        }
+        
+        if traitCollection.verticalSizeClass == .regular {
+            orientationLabel.text = "portrait"
+        }
+
+    }
 
 }
 
